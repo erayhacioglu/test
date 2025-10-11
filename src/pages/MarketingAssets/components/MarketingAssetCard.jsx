9462f09414803eb5ec87c020333b3bde01b2a57a
erayhacioglu <77465771+erayhacioglu@users.noreply.github.com>
2025-10-11 18:27:30 +0300

    marketing assets yeni sekmede açma

--- src/pages/MarketingAssets/components/MarketingAssetCard.jsx
+++ src/pages/MarketingAssets/components/MarketingAssetCard.jsx
@@ -1,23 +1,35 @@
 import { useDispatch, useSelector } from "react-redux";
 import trash from "../../../assets/img/icons/trash.svg";
 import cardBackground from "../../../assets/img/marketing_assets_background.jpg";
-import { deleteMarketingAssetsData, getMarketingAssetsData } from "../../../redux/slices/MarketingAssetsSlice";
+import {
+  deleteMarketingAssetsData,
+  getMarketingAssetsData,
+} from "../../../redux/slices/MarketingAssetsSlice";
 
-const MarketingAssetCard = ({isUpdated,data}) => {
+const MarketingAssetCard = ({ isUpdated, data }) => {
   const dispatch = useDispatch();
-  const {user} = useSelector(state => state.user);
+  const { user } = useSelector((state) => state.user);
   const cardId = user?.cardId;
 
   const handleDeleteItem = async (id) => {
-    const res = await dispatch(deleteMarketingAssetsData({catalogId:id}));
-    if(res?.meta?.requestStatus === "fulfilled"){
+    const res = await dispatch(deleteMarketingAssetsData({ catalogId: id }));
+    if (res?.meta?.requestStatus === "fulfilled") {
       dispatch(getMarketingAssetsData({ cardId }));
     }
-  }
+  };
+
+  const handleClickCard = (url) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
 
   return (
-    <div className={`marketing_assets_card_container ${isUpdated ? "update":""}`}>
-      <div className="marketing_assets_card">
+    <div
+      className={`marketing_assets_card_container ${isUpdated ? "update" : ""}`}
+    >
+      <div
+        className="marketing_assets_card"
+        onClick={() => handleClickCard(data?.url)}
+      >
         <div className="marketing_assets_card_header">
           <img
             src={data?.coverPhoto ?? cardBackground}
@@ -26,13 +38,18 @@ const MarketingAssetCard = ({isUpdated,data}) => {
           />
         </div>
         <div className="marketing_assets_card_content">
-          <p className="marketing_assets_card_content_text">
-            {data?.name}
-          </p>
+          <p className="marketing_assets_card_content_text">{data?.name}</p>
         </div>
       </div>
-      <button className="marketing_assets_card_delete_button" onClick={() => handleDeleteItem(data?.id)}>
-        <img src={trash} alt="" className="marketing_assets_card_delete_button_img"/>
+      <button
+        className="marketing_assets_card_delete_button"
+        onClick={() => handleDeleteItem(data?.id)}
+      >
+        <img
+          src={trash}
+          alt=""
+          className="marketing_assets_card_delete_button_img"
+        />
       </button>
     </div>
   );
